eb93445106895f92459aac3b8fdea2eb80760642
Artem Andreev <missmao23@yandex.ru>
2022-07-08 17:02:41 +0300

    feat: сделать адаптивность и JS функционал

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,22 @@ function closePopup() {
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+
+let formElement = document.querySelector('.user-form');
+let nameInput = document.querySelector('.input-name');
+let jobInput = document.querySelector('.input-job');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+  let userName = document.querySelector('.user-info__name');
+  let userJob = document.querySelector('.user-info__job');
+
+ userName.textContent = nameValue;
+ userJob.textContent = jobValue;
+ closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
